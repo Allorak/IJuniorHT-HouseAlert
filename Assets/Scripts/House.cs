fd7c0704f5c563a1bf41bffa0d52b2d445393980
Allorak <allorakspam@gmail.com>
2023-07-19 02:28:03 +0300

    Indentation fix try

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -3,18 +3,18 @@ using UnityEngine.Events;
 
 public class House : MonoBehaviour
 {
-	[SerializeField] private UnityEvent _playerEntered;
-	[SerializeField] private UnityEvent _playerExited;
+    [SerializeField] private UnityEvent _playerEntered;
+    [SerializeField] private UnityEvent _playerExited;
 
-	private void OnTriggerEnter2D(Collider2D other)
-	{
-		if(other.TryGetComponent(out Player player))
-			_playerEntered?.Invoke();
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.TryGetComponent(out Player player))
+            _playerEntered?.Invoke();
     }
 
-	private void OnTriggerExit2D(Collider2D other)
-	{
-		if(other.TryGetComponent(out Player player))
-			_playerExited?.Invoke();
-	}
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.TryGetComponent(out Player player))
+            _playerExited?.Invoke();
+    }
 }
